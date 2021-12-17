50146fd937f6b05a96d3c37d91a9f726701e5afc
Yed Star <chstokrp@gmail.com>
2021-12-17 14:46:30 -0800

    adjust divs

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-var now = moment();
+
+var now = moment().format('LLL');
+var timeOnly = $("currentDay");
+var currentHour = moment().format("HH");
+
 
 //header date
 var todaysDate = document.createElement('p');
 todaysDate.textContent = (now);
 currentDay.appendChild(todaysDate);
 
-var rowOne = document.createElement('div');
-rowOne.textContent = '8:00 AM';
-rowContainer.appendChild(rowOne);
 
-var rowTwo = document.createElement('div');
-rowTwo.textContent = '9:00 AM';
-rowContainer.appendChild(rowTwo);
+function storeLocal() {
+    allRows.querySelector('row');
+    
+}
 
+function saveChanges() {
+    saveButton.querySelector('saveBtn');
+    saveButton.addEventListener("click" , storeLocal);
+    console.log(saveChanges);
 
-
+}
 
