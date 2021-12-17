0e7023d9f1a2ea565200a3553cf0465cebee1a29
Yed Star <chstokrp@gmail.com>
2021-12-17 12:55:38 -0800

    add current date

---
+++ main.js
@@ -0,0 +1,18 @@
+var now = moment();
+
+//header date
+var todaysDate = document.createElement('p');
+todaysDate.textContent = (now);
+currentDay.appendChild(todaysDate);
+
+var rowOne = document.createElement('div');
+rowOne.textContent = '8:00 AM';
+rowContainer.appendChild(rowOne);
+
+var rowTwo = document.createElement('div');
+rowTwo.textContent = '9:00 AM';
+rowContainer.appendChild(rowTwo);
+
+
+
+
